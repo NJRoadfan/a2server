cba3801c03bf1506e7b49c38434481c8904a4ab6
NJRoadfan <NJRoadfan@users.noreply.github.com>
2026-06-22 20:06:54 -0400

    First run with TashTalk USB setup
    - Alter TashRouter setup so that it is a bit more graceful if someone doesn't plug in a USB device.
    
    - Add support for setting up a TashTalk USB adapter. This should work with any adapter as long as its USB device name string contains "TashTalk".
    
    - Note that this setup is flexible. You can run both a TashTalk Hat AND USB device at the same time if you want.

--- files/tashrouter.py
+++ files/tashrouter.py
@@ -2,11 +2,12 @@ import logging
 import time
 import signal
 import sys
+import os
 
 import tashrouter.netlog
 from tashrouter.port.ethertalk.tap import TapPort
 from tashrouter.port.localtalk.ltoudp import LtoudpPort
-#from tashrouter.port.localtalk.tashtalk import TashTalkPort
+from tashrouter.port.localtalk.tashtalk import TashTalkPort
 from tashrouter.router.router import Router
 
 def sigterm_handler(_signo, _stack_frame):
@@ -15,12 +16,22 @@ def sigterm_handler(_signo, _stack_frame):
 
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 #tashrouter.netlog.set_log_str_func(logging.debug)  # comment this line for speed and reduced spam
+portlist = [LtoudpPort(seed_network=5, seed_zone_name=b'A2SERVER')]
+portlist.append(TapPort(tap_name='tash0', hw_addr=b'\xAA\xBB\xCC\xDD\x11\x22', seed_network_min=6502, seed_network_max=6502, seed_zone_names=[b'A2SERVER']))
 
-router = Router('router', ports=(
-  LtoudpPort(seed_network=5, seed_zone_name=b'A2SERVER'),
-  #TashTalkPort(serial_port='/dev/ttyAMA0', seed_network=6, seed_zone_name=b'A2SERVER'),
-  TapPort(tap_name='tash0', hw_addr=b'\xAA\xBB\xCC\xDD\x11\x22', seed_network_min=6502, seed_network_max=6502, seed_zone_names=[b'A2SERVER']),
-))
+tashtalkusbdevice=None
+tashtalkhatdevice=None
+
+if tashtalkusbdevice is not None:
+    if os.path.exists(tashtalkusbdevice):
+        portlist.append(TashTalkPort(serial_port=tashtalkusbdevice, seed_network=6, seed_zone_name=b'A2SERVER'))
+
+if tashtalkhatdevice is not None:
+    if os.path.exists(tashtalkhatdevice):
+        portlist.append(TashTalkPort(serial_port=tashtalkhatdevice, seed_network=7, seed_zone_name=b'A2SERVER'))
+
+router = Router('router', ports=portlist)
+
 print('router away!')
 router.start()
 signal.signal(signal.SIGTERM, sigterm_handler)
