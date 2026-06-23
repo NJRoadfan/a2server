bff19076fca40ca432f2ae50a2372480444d593b
NJRoadfan <NJRoadfan@users.noreply.github.com>
2026-06-23 16:55:32 -0400

    Auto detect TashTalk USB
    Update TashRouter to autodetect TashTalk USB devices so the user isn't prompted. This should avoid shifting port device assignments.

--- files/tashrouter.py
+++ files/tashrouter.py
@@ -3,6 +3,8 @@ import time
 import signal
 import sys
 import os
+import serial
+import serial.tools.list_ports
 
 import tashrouter.netlog
 from tashrouter.port.ethertalk.tap import TapPort
@@ -14,12 +16,28 @@ def sigterm_handler(_signo, _stack_frame):
     # Raises SystemExit(0):
     sys.exit(0)
 
+def find_usb_serial(target_name):
+    ports = serial.tools.list_ports.grep(target_name)
+
+    for port in ports:
+        # Match using product string
+        if port.product:
+            return port.device
+
+    return None
+
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 #tashrouter.netlog.set_log_str_func(logging.debug)  # comment this line for speed and reduced spam
 portlist = [LtoudpPort(seed_network=5, seed_zone_name=b'A2SERVER')]
 portlist.append(TapPort(tap_name='tash0', hw_addr=b'\xAA\xBB\xCC\xDD\x11\x22', seed_network_min=6502, seed_network_max=6502, seed_zone_names=[b'A2SERVER']))
 
-tashtalkusbdevice=None
+TARGET_VID = 0x10C4
+TARGET_PID = 0xEA60
+TARGET_NAME = 'tashtalk'
+
+#Autodetect TashTalk USB
+#If your device has different identifiers, you can change them above
+tashtalkusbdevice=find_usb_serial(TARGET_NAME)
 tashtalkhatdevice=None
 
 if tashtalkusbdevice is not None:
